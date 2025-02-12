a0f54cec39f56d32c16d4d2017b4b49e0efeaca7
AndriesDK <andriesd@cashcrusaders.co.za>
2025-02-12 11:59:10 +0200

    setup linter

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
-import {SafeAreaView, Text, StatusBar} from 'react-native';
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
 
 export default function HomeScreen() {
   return (
-      <SafeAreaView className={"flex-1 items-center justify-center bg-white"}>
-          <Text className={"text-red-500"}>Uber Clone!!</Text>
-          <StatusBar style="auto" />
-      </SafeAreaView>
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <Text className="text-red-700">Uber Clone!!</Text>
+      <StatusBar style="auto" />
+    </SafeAreaView>
   );
 }
