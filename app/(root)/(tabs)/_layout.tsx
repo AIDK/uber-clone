23a6be958b1c59195e436044b024e581de2f4981
AndriesDK <andriesd@cashcrusaders.co.za>
2025-02-13 13:39:01 +0200

    added auto navigate to home layout after user authenticates

---
+++ app/(root)/(tabs)/_layout.tsx
@@ -0,0 +1,92 @@
+import { Tabs } from "expo-router";
+import { Image, ImageSourcePropType, Platform, View } from "react-native";
+import { icons } from "@/constants";
+
+const TabIcon = ({
+  source,
+  focused,
+}: {
+  source: ImageSourcePropType;
+  focused: boolean;
+}) => (
+  <View
+    className={`flex flex-row w-12 h-12 justify-center items-center rounded-full ${focused ? "bg-general-400" : ""}`}
+  >
+    <Image
+      source={source}
+      resizeMode={"contain"}
+      tintColor={"white"}
+      className={"w-7 h-7"}
+    />
+  </View>
+);
+
+const osPadding = Platform.OS === "ios" ? 30 : 0;
+
+const Layout = () => (
+  <Tabs
+    initialRouteName={"home"}
+    screenOptions={{
+      tabBarActiveTintColor: "white",
+      tabBarInactiveTintColor: "white",
+      tabBarShowLabel: false,
+      tabBarStyle: {
+        backgroundColor: "#333333",
+        paddingBottom: osPadding,
+        borderRadius: 50,
+        overflow: "hidden",
+        marginHorizontal: 20,
+        marginBottom: 20,
+        height: 78,
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        flexDirection: "row",
+        position: "absolute",
+      },
+    }}
+  >
+    <Tabs.Screen
+      name={"home"}
+      options={{
+        title: "Home",
+        headerShown: false,
+        tabBarIcon: ({ focused }) => (
+          <TabIcon focused={focused} source={icons.home} />
+        ),
+      }}
+    />
+    <Tabs.Screen
+      name={"rides"}
+      options={{
+        title: "Rides",
+        headerShown: false,
+        tabBarIcon: ({ focused }) => (
+          <TabIcon focused={focused} source={icons.list} />
+        ),
+      }}
+    />
+    <Tabs.Screen
+      name={"chat"}
+      options={{
+        title: "Chat",
+        headerShown: false,
+        tabBarIcon: ({ focused }) => (
+          <TabIcon focused={focused} source={icons.chat} />
+        ),
+      }}
+    />
+    <Tabs.Screen
+      name={"profile"}
+      options={{
+        title: "Profile",
+        headerShown: false,
+        tabBarIcon: ({ focused }) => (
+          <TabIcon focused={focused} source={icons.profile} />
+        ),
+      }}
+    />
+  </Tabs>
+);
+
+export default Layout;
