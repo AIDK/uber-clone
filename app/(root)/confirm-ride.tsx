0d33ac4c6abd257ad92fed3032f99adf216e19b5
AndriesDK <andriesd@cashcrusaders.co.za>
2025-02-15 20:02:29 +0200

    implemented BottomSheetView to find rides

---
+++ app/(root)/confirm-ride.tsx
@@ -0,0 +1,11 @@
+﻿import { Text, View } from "react-native";
+
+const ConfirmRide = () => {
+  return (
+    <View>
+      <Text>Confirm Ride</Text>
+    </View>
+  );
+};
+
+export default ConfirmRide;
--- app/(root)/find-ride.tsx
+++ app/(root)/find-ride.tsx
@@ -1,5 +1,10 @@
 ﻿import { View, Text } from "react-native";
 import { useLocationStore } from "@/store";
+import RideLayout from "@/components/RideLayout";
+import GoogleTextInput from "@/components/GoogleTextInput";
+import { icons } from "@/constants";
+import CustomButton from "@/components/CustomButton";
+import { router } from "expo-router";
 
 const FindRide = () => {
   const {
@@ -10,10 +15,36 @@ const FindRide = () => {
   } = useLocationStore();
 
   return (
-    <View>
-      <Text className={"text-2xl"}>You are here: {userAddress}</Text>
-      <Text className={"text-2xl"}>You are going to: {destinationAddress}</Text>
-    </View>
+    <RideLayout title={"Ride"} snapPoints={["85%"]}>
+      {/* From input */}
+      <View className={"my-3"}>
+        <Text className={"text-lg font-JakartaSemiBold mb-3"}>From</Text>
+        <GoogleTextInput
+          icon={icons.target}
+          initialLocation={userAddress!}
+          containerStyle={"bg-neutral-100"}
+          textInputBackgroundColor={"$#f5f5f5"}
+          handlePress={(location) => setUserLocation(location)}
+        />
+      </View>
+
+      {/* To input */}
+      <View className={"my-3"}>
+        <Text className={"text-lg font-JakartaSemiBold mb-3"}>To</Text>
+        <GoogleTextInput
+          icon={icons.map}
+          initialLocation={destinationAddress!}
+          containerStyle={"bg-neutral-100"}
+          textInputBackgroundColor={"$#f5f5f5"}
+          handlePress={(location) => setDestinationLocation(location)}
+        />
+      </View>
+      <CustomButton
+        title={"Find now"}
+        onPress={() => router.push("/(root)/confirm-ride")}
+        className={"mt-5"}
+      />
+    </RideLayout>
   );
 };
 
