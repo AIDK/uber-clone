dfed47a5b8cc6c5e170a752232803b47151c33b0
AndriesDK <andriesd@cashcrusaders.co.za>
2025-02-15 13:40:31 +0200

    implemented additional styling to RideCard, added location search component, added current location component

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,23 +1,34 @@
-﻿import { FlatList } from "react-native";
+﻿import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { useUser } from "@clerk/clerk-expo";
 import { SafeAreaView } from "react-native-safe-area-context";
 import RideCard from "@/components/RideCard";
+import { icons, images } from "@/constants";
+import GoogleTextInput from "@/components/GoogleTextInput";
+import Map from "@/components/Map";
 
 const recentRides = [
   {
     ride_id: "1",
     origin_address: "Kathmandu, Nepal",
     destination_address: "Pokhara, Nepal",
-    origin_latitude: "27.717245",
-    origin_longitude: "85.323961",
-    destination_latitude: "28.209583",
-    destination_longitude: "83.985567",
+    origin_latitude: 27.717245,
+    origin_longitude: 85.323961,
+    destination_latitude: 28.209583,
+    destination_longitude: 83.985567,
     ride_time: 391,
-    fare_price: "19500.00",
+    fare_price: 19500.0,
     payment_status: "paid",
     driver_id: 2,
     user_id: "1",
     created_at: "2024-08-12 05:19:20.620007",
+    user_email: "david@example.com",
     driver: {
       driver_id: "2",
       first_name: "David",
@@ -27,23 +38,24 @@ const recentRides = [
       car_image_url:
         "https://ucarecdn.com/a3872f80-c094-409c-82f8-c9ff38429327/-/preview/930x932/",
       car_seats: 5,
-      rating: "4.60",
+      rating: 4.6,
     },
   },
   {
     ride_id: "2",
     origin_address: "Jalkot, MH",
     destination_address: "Pune, Maharashtra, India",
-    origin_latitude: "18.609116",
-    origin_longitude: "77.165873",
-    destination_latitude: "18.520430",
-    destination_longitude: "73.856744",
+    origin_latitude: 18.609116,
+    origin_longitude: 77.165873,
+    destination_latitude: 18.52043,
+    destination_longitude: 73.856744,
     ride_time: 491,
-    fare_price: "24500.00",
+    fare_price: 24500.0,
     payment_status: "paid",
     driver_id: 1,
     user_id: "1",
     created_at: "2024-08-12 06:12:17.683046",
+    user_email: "james@example.com",
     driver: {
       driver_id: "1",
       first_name: "James",
@@ -53,23 +65,24 @@ const recentRides = [
       car_image_url:
         "https://ucarecdn.com/a2dc52b2-8bf7-4e49-9a36-3ffb5229ed02/-/preview/465x466/",
       car_seats: 4,
-      rating: "4.80",
+      rating: 4.8,
     },
   },
   {
     ride_id: "3",
     origin_address: "Zagreb, Croatia",
     destination_address: "Rijeka, Croatia",
-    origin_latitude: "45.815011",
-    origin_longitude: "15.981919",
-    destination_latitude: "45.327063",
-    destination_longitude: "14.442176",
+    origin_latitude: 45.815011,
+    origin_longitude: 15.981919,
+    destination_latitude: 45.327063,
+    destination_longitude: 14.442176,
     ride_time: 124,
-    fare_price: "6200.00",
+    fare_price: 6200.0,
     payment_status: "paid",
     driver_id: 1,
     user_id: "1",
     created_at: "2024-08-12 08:49:01.809053",
+    user_email: "james@example.com",
     driver: {
       driver_id: "1",
       first_name: "James",
@@ -79,23 +92,24 @@ const recentRides = [
       car_image_url:
         "https://ucarecdn.com/a2dc52b2-8bf7-4e49-9a36-3ffb5229ed02/-/preview/465x466/",
       car_seats: 4,
-      rating: "4.80",
+      rating: 4.8,
     },
   },
   {
     ride_id: "4",
     origin_address: "Okayama, Japan",
     destination_address: "Osaka, Japan",
-    origin_latitude: "34.655531",
-    origin_longitude: "133.919795",
-    destination_latitude: "34.693725",
-    destination_longitude: "135.502254",
+    origin_latitude: 34.655531,
+    origin_longitude: 133.919795,
+    destination_latitude: 34.693725,
+    destination_longitude: 135.502254,
     ride_time: 159,
-    fare_price: "7900.00",
+    fare_price: 7900.0,
     payment_status: "paid",
     driver_id: 3,
     user_id: "1",
     created_at: "2024-08-12 18:43:54.297838",
+    user_email: "michael@example.com",
     driver: {
       driver_id: "3",
       first_name: "Michael",
@@ -105,19 +119,100 @@ const recentRides = [
       car_image_url:
         "https://ucarecdn.com/289764fb-55b6-4427-b1d1-f655987b4a14/-/preview/930x932/",
       car_seats: 4,
-      rating: "4.70",
+      rating: 4.7,
     },
   },
 ];
 
 export default function Page() {
   const { user } = useUser();
+  const isLoading = true;
+
+  const handleSignOut = () => {};
+  const handleDestinationPress = () => {};
 
   return (
     <SafeAreaView className={"bg-general-500"}>
       <FlatList
         data={recentRides?.slice(0, 5)}
         renderItem={({ item }) => <RideCard ride={item} />}
+        className={"px-5"} // left and right padding of 20px
+        keyboardShouldPersistTaps={"handled"}
+        contentContainerStyle={{
+          paddingBottom: 80, // leaves gap at bottom of list
+        }}
+        // displays when list is empty (no data)
+        ListEmptyComponent={() => (
+          <View className={"flex flex-col items-center justify-center"}>
+            {!isLoading ? (
+              // display image when list is empty
+              <>
+                <Image
+                  source={images.noResult}
+                  className={"w-40 h-40"}
+                  alt={"No recent rides found"}
+                  resizeMode={"contain"}
+                />
+                <Text className={"text-sm text-gray-400"}>
+                  No recent rides found
+                </Text>
+              </>
+            ) : (
+              // loading indicator (spinner)
+              <ActivityIndicator
+                className={"pt-5"}
+                size={"small"}
+                color={"#000"}
+              />
+            )}
+          </View>
+        )}
+        // render text at the top of the list (allows for logout)
+        ListHeaderComponent={() => (
+          <>
+            <View className={"flex flex-row items-center justify-between my-5"}>
+              {/* welcome text (can display the user's name or email */}
+              <Text className={"text-xl font-JakartaExtraBold capitalize"}>
+                Welcome{", "}
+                {user?.firstName ||
+                  user?.emailAddresses[0].emailAddress.split("@")[0]}{" "}
+                👋
+              </Text>
+
+              {/* logout button */}
+              <TouchableOpacity
+                onPress={handleSignOut}
+                className={
+                  "justify-center items-center w-10 h-10 rounded-full bg-white"
+                }
+              >
+                <Image source={icons.out} className={"w-4 h-4"} />
+              </TouchableOpacity>
+            </View>
+
+            {/* Google text input */}
+            <GoogleTextInput
+              icon={icons.search}
+              containerStyle={"bg-white shadow-md shadow-neutral-300"}
+              handlePress={handleDestinationPress}
+            />
+            <>
+              <Text className={"text-xl font-JakartaBold mt-5 mb-3"}>
+                Your Current Location
+              </Text>
+              <View
+                className={
+                  "flex flex-row items-center bg-transparent h-[300px]"
+                }
+              >
+                <Map />
+              </View>
+            </>
+            <Text className={"text-xl font-JakartaBold mt-5 mb-3"}>
+              Recent Rides
+            </Text>
+          </>
+        )}
       />
     </SafeAreaView>
   );
---
+++ components/Map.tsx
@@ -0,0 +1,9 @@
+﻿import { View, Text } from "react-native";
+
+const Map = () => (
+  <View>
+    <Text>Map</Text>
+  </View>
+);
+
+export default Map;
